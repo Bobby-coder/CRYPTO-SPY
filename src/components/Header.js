f5aa030d90d445a6ae2c711abf77a7c588f388e3
Bobby-coder <bobbysadhwani612@gmail.com>
2022-10-20 17:30:34 +0530

    header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 const Header = () => {
-    return(
-        <>
-        <p>Header</p>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="h-14 bg-gray-80 px-5% flex items-center justify-between">
+        <p className="text-xl tracking-wide text-blue font-semibold">CRYPTO SPY</p>
+        <div className="w-20 flex justify-center bg-gray-100 rounded">
+        <select className="text-lg outline-0 h-10 w-16 bg-gray-100 cursor-pointer">
+          <option value={'INR'}>INR</option>
+          <option value={'USD'}>USD</option>
+        </select>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Header;

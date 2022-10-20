1ecd730c8fda9d1099235ee45bd6cb30276509e7
Bobby-coder <bobbysadhwani612@gmail.com>
2022-10-20 21:56:13 +0530

    dropdown ui changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,28 @@ const Header = () => {
   return (
     <>
       <div className="h-14 bg-gray-80 px-5% flex items-center justify-between">
-        <p className="text-xl tracking-wide text-blue font-semibold">CRYPTO SPY</p>
-        <div className="w-20 flex justify-center bg-gray-100 rounded">
-        <select className="text-lg outline-0 h-10 w-16 bg-gray-100 cursor-pointer">
-          <option value={'INR'}>INR</option>
-          <option value={'USD'}>USD</option>
+        <p className="text-xl tracking-wide text-blue font-semibold">
+          CRYPTO SPY
+        </p>
+
+        <select
+          class="
+      appearance-none
+      w-20
+      text-center
+      py-2
+      text-xl
+      font-normal
+      bg-gray-100
+      rounded
+      transition
+      ease-in-out
+      outline-none"
+          
+        >
+          <option value={"INR"}>₹ INR</option>
+          <option value={"USD"}>$ USD</option>
         </select>
-        </div>
       </div>
     </>
   );

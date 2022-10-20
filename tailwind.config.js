4192abb4bb8c1963a65770d109e5aebe1055e751
Bobby-coder <bobbysadhwani612@gmail.com>
2022-10-20 17:31:57 +0530

    tailwind css added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'black': "#101623",
+        'gray-80': '#2d3446',
+        'gray-100': '#101623',
+        'white': "#ffffff",
+        'red': "#E21717",
+      },
+
+      padding: {
+        '5%': '5%',
+      }
+    },
+  },
+  plugins: [],
+};

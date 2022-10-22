e79438346e4f041fb495e68441321faf6bf7ee50
Bobby-coder <bobbysadhwani612@gmail.com>
2022-10-22 17:56:08 +0530

    CoinsTable and Pagination component displayed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,13 @@
-import { useEffect, useState } from 'react';
-import axios from "axios";
-import { CryptoState } from './ContextProvider';
-import Slider from './Slider';
+import Banner from './Banner';
+import CoinsTable from './CoinsTable';
+import Pagination from './Pagination';
 
 const Home = () => {
-    const {currency} = CryptoState()
     return(
         <>
-        <div className='flex flex-col items-center px-[5%] pt-8 h-[400px] bg-hero-pattern'>
-            <h1 className="text-5xl font-semibold mb-2 text-center">CRYPTO SPY</h1>
-            <p className='text-sm tracking-wide opacity-50 text-center'>Get All Details About Your Favorite Crypto Currency</p>
-            <Slider />
-        </div>
+        <Banner />
+        <CoinsTable />
+        <Pagination />
         </>
     )
 }

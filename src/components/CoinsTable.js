3d5e65776c2c908fa9592ef8e4b8aad7170f19b9
Bobby-coder <bobbysadhwani612@gmail.com>
2022-10-22 23:45:08 +0530

    search bug fixed

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -4,9 +4,9 @@ import { useCryptoState } from "./ContextProvider";
 import { numberWithCommas } from "./Slider";
 
 const CoinsTable = () => {
-  const { currency, symbol,page } = useCryptoState();
+  const { currency, symbol, page } = useCryptoState();
   const [tableCoins, setTableCoins] = useState([]);
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
 
   const getTableCoins = async () => {
     const res = await axios.get(
@@ -21,13 +21,10 @@ const CoinsTable = () => {
 
   const handleSearch = () => {
     return tableCoins.filter(
-        (coin) =>
-         {if (search === '') {
-           return tableCoins
-         } else {
-          return coin.name.toLowerCase().includes(search.trim())
-         }
-        })
+      (coin) =>
+        coin.name.toLowerCase().includes(search.toLowerCase().trim()) ||
+        coin.symbol.toLowerCase().includes(search.toLowerCase().trim())
+    );
   };
 
   return (
@@ -37,9 +34,8 @@ const CoinsTable = () => {
           type="text"
           placeholder="Search for a crypto currency"
           className="w-full bg-transparent h-full border-solid border-gray-500 border-2 outline-none px-5 hover:border-gray-50 capitalize placeholder:uppercase"
-          onChange={(e) => setSearch(e.target.value) }
+          onChange={(e) => setSearch(e.target.value)}
         />
-        
       </div>
 
       <div className="overflow-x-auto relative w-[90%] mx-auto">
@@ -61,44 +57,46 @@ const CoinsTable = () => {
             </tr>
           </thead>
           <tbody>
-            {handleSearch().slice((page-1)*10, (page-1)*10+10).map((coinObj) => {
-              let profit = coinObj.price_change_percentage_24h;
-              return (
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer hover:bg-gray-100 hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white">
-                  <td
-                    scope="row"
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    <div className="flex gap-3 items-center justify-start">
-                      <img src={coinObj.image} className="h-[50px]" />
-                      <div className="flex flex-col">
-                        <p className="uppercase text-xl">{coinObj.symbol}</p>
-                        <p>{coinObj.name}</p>
+            {handleSearch()
+              .slice((page - 1) * 10, (page - 1) * 10 + 10)
+              .map((coinObj) => {
+                let profit = coinObj.price_change_percentage_24h;
+                return (
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer hover:bg-gray-100 hover:text-gray-700 dark:hover:bg-gray-700 dark:hover:text-white">
+                    <td
+                      scope="row"
+                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      <div className="flex gap-3 items-center justify-start">
+                        <img src={coinObj.image} className="h-[50px]" />
+                        <div className="flex flex-col">
+                          <p className="uppercase text-xl">{coinObj.symbol}</p>
+                          <p>{coinObj.name}</p>
+                        </div>
                       </div>
-                    </div>
-                  </td>
-                  <td className="py-4 text-center text-[16px] font-semibold">
-                    {` ${symbol} ${numberWithCommas(coinObj.current_price)}`}
-                  </td>
-                  <td
-                    className={
-                      profit > 0
-                        ? "py-4 text-center text-[16px] text-green font-semibold"
-                        : "py-4 text-center text-[16px] text-red font-semibold"
-                    }
-                  >
-                    {`${
-                      profit > 0 ? `+${profit.toFixed(2)}` : profit.toFixed(2)
-                    } %`}
-                  </td>
-                  <td className="py-4 text-[16px] font-semibold text-center">
-                    <span>{symbol}</span>{" "}
-                    {coinObj.market_cap.toString().slice(0, -6)}
-                    <span>M</span>
-                  </td>
-                </tr>
-              );
-            })}
+                    </td>
+                    <td className="py-4 text-center text-[16px] font-semibold">
+                      {` ${symbol} ${numberWithCommas(coinObj.current_price)}`}
+                    </td>
+                    <td
+                      className={
+                        profit > 0
+                          ? "py-4 text-center text-[16px] text-green font-semibold"
+                          : "py-4 text-center text-[16px] text-red font-semibold"
+                      }
+                    >
+                      {`${
+                        profit > 0 ? `+${profit.toFixed(2)}` : profit.toFixed(2)
+                      } %`}
+                    </td>
+                    <td className="py-4 text-[16px] font-semibold text-center">
+                      <span>{symbol}</span>{" "}
+                      {coinObj.market_cap.toString().slice(0, -6)}
+                      <span>M</span>
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>

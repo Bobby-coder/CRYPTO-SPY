9ef62cb5a813d5d6c86b31abb914f6ad4bdae9fb
Bobby-coder <bobbysadhwani612@gmail.com>
2022-10-22 17:57:26 +0530

    Slider component UI fixed

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AliceCarousel from "react-alice-carousel";
-import { CryptoState } from "./ContextProvider";
+import { useCryptoState } from "./ContextProvider";
+import { Link } from "react-router-dom";
 
 export function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 const Slider = () => {
-  const { symbol, currency } = CryptoState();
+  const { symbol, currency } = useCryptoState();
   const [trendingCoins, setTrendingCoins] = useState([]);
 
   const getTrendingCoins = async () => {
@@ -26,7 +27,9 @@ const Slider = () => {
     let profit = coinObj.price_change_percentage_24h;
     return (
       <div className="flex flex-col items-center justify-between ">
+        <Link to = 'coins/:id' >
         <img src={coinObj.image} className="h-20 mb-3" />
+        </Link>
         <p className="text-center text-lg uppercase font-semibold">
           {coinObj.symbol} &nbsp;
           <span className={profit < 0 ? "text-red font-semibold" : "text-green font-semibold"}>

37f5ab093f477239deeb0ac3f02c14d0daf8960e
Bobby-coder <bobbysadhwani612@gmail.com>
2022-11-28 23:02:29 +0530

    Coin Graph imported

--- src/components/coinpage/CoinInfo.js
+++ src/components/coinpage/CoinInfo.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useParams } from "react-router-dom";
 import { useCryptoState } from "../../context/ContextProvider";
 import { numberWithCommas } from "../home/Slider";
+import parse from 'html-react-parser';
+import CoinGraph from "./CoinGraph";
 
 const CoinInfo = () => {
     const [singleCoin, setSingleCoin] = useState([])
@@ -21,15 +23,16 @@ const CoinInfo = () => {
     )
     return(
         <>
-        <div className="py-5 px-[5%] flex flex-col xl:flex-row">
-        <div className="flex flex-col gap-2 xl:w-[30%]">
+        <div className="flex flex-col gap-[30px] py-[20px]">
+        <div className="flex flex-col gap-2 w-full px-[2%]">
             <img src={singleCoin.image?.large} className='h-32 self-center'/>
             <h1 className="text-center text-3xl font-semibold uppercase">{singleCoin.name}</h1>
-            <p className="text-justify opacity-70">{singleCoin.description?.en.split('.')[0]}.</p>
-            <p className="tracking-wide font-bold">Rank: <span className="opacity-70 font-normal">{singleCoin.market_cap_rank}</span></p>
-            <p className="tracking-wide font-bold">Current Price: <span className="opacity-70 font-normal" >{symbol}{numberWithCommas(singleCoin.market_data?.current_price[currency.toLowerCase()])}</span></p>
-            <p className="tracking-wide font-bold">Market Cap: <span className="opacity-70 font-normal" >{numberWithCommas(singleCoin.market_data?.market_cap[currency.toLowerCase()].toString().slice(0, -6))}M</span></p>
+            {parse(`<p className="text-justify lg:text-[20px] opacity-70">${singleCoin.description?.en.split('. ')[0]}. ${singleCoin.description?.en.split('. ')[1]}. ${singleCoin.description?.en.split('. ')[2]}.</p>`)}
+            <p className="tracking-wide font-bold lg:text-[20px]">Rank: <span className="opacity-70 font-normal">{singleCoin.market_cap_rank}</span></p>
+            <p className="tracking-wide font-bold lg:text-[20px]">Current Price: <span className="opacity-70 font-normal" >{symbol}{numberWithCommas(singleCoin.market_data?.current_price[currency.toLowerCase()])}</span></p>
+            <p className="tracking-wide font-bold lg:text-[20px]">Market Cap: <span className="opacity-70 font-normal" >{numberWithCommas(singleCoin.market_data?.market_cap[currency.toLowerCase()].toString().slice(0, -6))}M</span></p>
         </div>
+        <CoinGraph />
         </div>
         </>
     )

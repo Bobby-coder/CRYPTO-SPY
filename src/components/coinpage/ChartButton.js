f21b9a95c1ae5f84dfcb9620ec83465dcd9ca7d4
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-08 00:20:14 +0530

    Button UI changed

--- src/components/coinpage/ChartButton.js
+++ src/components/coinpage/ChartButton.js
@@ -1,28 +1,20 @@
-import { TiChartLine } from "react-icons/ti";
 import { useCryptoState } from "../../context/ContextProvider";
 
 const ChartButton = ({ days }) => {
   const { setDays } = useCryptoState();
+  const paraStyle = 'mx-auto font-semibold'
   return (
     <>
       <button
         type="button"
-        class="text-gray-900 bg-[#EEBC1D] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 mr-2 mb-2"
+        class="text-gray-900 bg-[#EEBC1D] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-sm text-sm px-2 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 mb-2 grow"
         onClick={() => setDays(days)}
       >
-        <TiChartLine />
-        {
-            days === 1 && <p>24 HOURS</p>
-        }
-        {
-            days === 30 && <p>1 MONTH</p>
-        }
-        {
-            days === 180 && <p>6 MONTHS</p>
-        }
-        {
-            days === 365 && <p>1 YEAR</p>
-        }
+        
+        {days === 1 && <p className={paraStyle}>24 HOURS</p>}
+        {days === 30 && <p className={paraStyle}>1 MONTH</p>}
+        {days === 180 && <p className={paraStyle}>6 MONTHS</p>}
+        {days === 365 && <p className={paraStyle}>1 YEAR</p>}
       </button>
     </>
   );
